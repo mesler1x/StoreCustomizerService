2d9902c500339972a1bbf7cbff7955fd9e787a9f
Roman Mesler <meslerdev@gmail.com>
2026-01-13 09:30:05 +0500

    WS-3 Add theme service

--- src/main/java/ru/urfu/storagecustomizerservice/controller/ThemeController.java
+++ src/main/java/ru/urfu/storagecustomizerservice/controller/ThemeController.java
@@ -1,5 +1,7 @@
 package ru.urfu.storagecustomizerservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +11,7 @@ import ru.urfu.storagecustomizerservice.service.ThemeService;
 import java.util.List;
 import java.util.UUID;
 
+@Tag(name = "Сервис цветовых тем магазина")
 @CrossOrigin(originPatterns = {"http://localhost:3000", "*"})
 @RestController
 @RequiredArgsConstructor
@@ -18,18 +21,21 @@ public class ThemeController {
 
     private final ThemeService themeService;
 
+    @Operation(summary = "Получить активированную тему")
     @GetMapping("/active")
     public ThemeDto getActiveTheme() {
         log.info("Получен запрос на активацию темы");
         return themeService.getActiveTheme();
     }
 
+    @Operation(summary = "Получить список всех тем")
     @GetMapping
     public List<ThemeDto> getAllThemes() {
         log.info("Получен запрос на получение всех тем");
         return themeService.getAllThemes();
     }
 
+    @Operation(summary = "Применить выбор темы")
     @PutMapping("/{themeId}")
     public void chooseTheme(@PathVariable UUID themeId) {
         log.info("Получен запрос на установку темы");
